1100b0b5e454433092deddcacf4c4c86b84bbc65
RobertOLtean314 <olteanrobertadrian314@gmail.com>
2026-03-07 14:02:19 +0200

    Checkpoint from VS Code for coding agent session

--- server/src/handlers/persoana_fizica_handlers.rs
+++ server/src/handlers/persoana_fizica_handlers.rs
@@ -4,3 +4,5 @@ use actix_web::{HttpResponse, Responder, get};
 pub async fn persoana_fizica_handler() -> impl Responder {
     HttpResponse::Ok().body("Persoana Fizica - Fine!")
 }
+
+//TODO: Implement handlers for: CRUD operation that are calling CRUD services from persoana_fizica_services.rs
