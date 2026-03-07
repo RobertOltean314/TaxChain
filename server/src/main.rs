aa8f43399763f986b8b8b99d995f00632655be11
RobertOLtean314 <olteanrobertadrian314@gmail.com>
2026-03-07 13:55:42 +0200

    refactoring + implementing test for handers

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,20 +1,18 @@
-use crate::handlers::persoana_fizica_handlers::*;
-use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
-
-pub mod handlers;
-pub mod models;
-pub mod services;
-pub mod validators;
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello World")
-}
+use actix_web::{App, HttpServer, web};
+use taxchain::{
+    handlers::{persoana_fizica_handler, persoana_juridica_handler},
+    hello,
+};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(web::scope("/app").service(persoana_fizica_handler)))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(web::scope("/persoana-fizica").service(persoana_fizica_handler))
+            .service(web::scope("/persoana-juridica").service(persoana_juridica_handler))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
--- server/src/services/persoana_juridica_service.rs
+++ server/src/services/persoana_juridica_service.rs
@@ -1,1 +1,6 @@
+use actix_web::{HttpResponse, Responder, get};
 
+#[get("/find-all")]
+pub async fn find_all() -> impl Responder {
+    HttpResponse::Ok().body("Works fine! - PersoanaJuridica")
+}
